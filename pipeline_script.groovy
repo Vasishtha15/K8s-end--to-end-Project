244e071ff1fb6be95a39200ab8be6f391687afe8
Ayushi Vasishtha <56094875+Vasishtha15@users.noreply.github.com>
2023-11-29 21:54:45 +0530

    Update pipeline_script.groovy

--- pipeline_script.groovy
+++ pipeline_script.groovy
@@ -1,1 +1,26 @@
+pipeline {
+    agent any
+    stages{
+        stage ('Code Clone') {
+            steps {
+                git url : 'https://github.com/Vasishtha15/node-todo-cicd.git' , branch : 'master'
+            }
+        }
+        stage ('Build') {
+            steps {
+                sh 'docker build . -t  node-todo-cicd:latest'
+            }
+        }
+        stage ('Testing') {
+            steps {
+                echo 'testing'
+            }
+        }
+        stage ('Deploy') {
+            steps {
+                sh 'docker run -d -p 8000:8000 node-todo-cicd:latest'
+            }
+        }
+    }
+}
 
